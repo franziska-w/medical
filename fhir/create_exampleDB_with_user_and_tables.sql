e7fc9bd56102ce9fd73186461f430a20d6aeff69
Franziska W <37049628+franziska-w@users.noreply.github.com>
2019-10-18 15:20:55 +0200

    Create create_exampleDB_with_user_and_tables.sql

---
+++ fhir/create_exampleDB_with_user_and_tables.sql
@@ -0,0 +1,28 @@
+-- Create database exampleDB and user nifi with privileges
+CREATE DATABASE exampleDB;
+CREATE USER 'nifi'@'localhost' IDENTIFIED BY '<password>';
+GRANT ALL PRIVILEGES ON exampleDB . * TO 'nifi'@'localhost';
+
+-- Create tables to load data from NiFi
+USE exampleDB;
+CREATE TABLE medication_ingredient (
+     id NVARCHAR(255) NULL
+    ,coding_system NVARCHAR(255) NULL
+    ,coding_code NVARCHAR(255) NULL
+    ,coding_display NVARCHAR(255) NULL
+    ,numerator_value INT NULL
+    ,numerator_system NVARCHAR(255) NULL
+    ,numerator_code NVARCHAR(255) NULL
+    ,denominator_value INT NULL
+    ,denominator_system NVARCHAR(255) NULL
+    ,denominator_code NVARCHAR(255) NULL
+);
+CREATE TABLE medicationrequest (
+     id NVARCHAR(255) NULL
+    ,Med_id NVARCHAR(255) NULL
+    ,Med_code_system NVARCHAR(255) NULL
+    ,Med_code_code NVARCHAR(255) NULL
+    ,Med_code_display NVARCHAR(255) NULL
+    ,subject_reference NVARCHAR(255) NULL
+    ,subject_display NVARCHAR(255) NULL
+);
