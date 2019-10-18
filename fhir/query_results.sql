ed9954a4ed25ff4e9d96b113c1ffe8b47a997c3c
Franziska W <37049628+franziska-w@users.noreply.github.com>
2019-10-18 15:22:11 +0200

    Create query_results.sql

---
+++ fhir/query_results.sql
@@ -0,0 +1,13 @@
+-- Query results from exampleDB
+SELECT
+     mr.subject_reference AS patient_uri
+    ,mr.subject_display AS patient
+    ,mr.id AS medicationrequest_uri
+    ,mr.Med_id AS medication_uri
+    ,IF(
+         LENGTH(mi.coding_display) > 100,
+         CONCAT(LEFT(mi.coding_display, 100), '...'),
+         mi.coding_display
+      ) AS medication_ingredient_display
+FROM medication_ingredient AS mi
+JOIN medicationrequest mr ON mi.id = mr.Med_id;
