6f0fe4720246a8ad1040d0d561752cb8d5812561
Franziska W <37049628+franziska-w@users.noreply.github.com>
2019-10-21 10:10:33 +0200

    Update query_results.sql

--- fhir/query_results.sql
+++ fhir/query_results.sql
@@ -1,14 +1,14 @@
 -- Query results from exampleDB
 SELECT
      mr.subject_reference AS patient_uri
-    ,mr.subject_display AS patient
+    ,mr.subject_display AS patient_display
     ,mr.id AS medicationrequest_uri
     ,mr.status AS medicationrequest_status
     ,mr.authoredOn AS medicationrequest_authoredOn
     ,mr.Med_id AS medication_uri
     ,IF(
-         LENGTH(mi.coding_display) > 100,
-         CONCAT(LEFT(mi.coding_display, 100), '...'),
+         LENGTH(mi.coding_display) > 50,
+         CONCAT(LEFT(mi.coding_display, 50), '...'),
          mi.coding_display
       ) AS medication_ingredient_display
 FROM medication_ingredient AS mi
